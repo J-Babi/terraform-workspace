1d4df634b5a60377812879e3034ca8290bcbecdb
jb <babila.jul@gmail.com>
2024-08-20 08:20:45 -0400

    modified backend .tf

--- terraform-aws-ec2/backend.tf
+++ terraform-aws-ec2/backend.tf
@@ -1,12 +1,10 @@
-terraform {
-  backend "remote" {
-#    hostname     = "tfe.rogers.com"
-    hostname     = "https://app.terraform.io"
-#    organization = "aws"
-    organization = "babis-jul"
-    workspaces {
-        name = "aws-msr-cast-dev"
-    }
-  }
-  required_version = "1.0.1"
+terraform { 
+  cloud { 
+    
+    organization = "babis-jul" 
+
+    workspaces { 
+      name = "aws-msr-cast-dev" 
+    } 
+  } 
 }
